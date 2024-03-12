28838edacb24d26df21abfeaf73acf22b888552a
vendeliin <vendelinkukla@gmial.com>
2024-03-12 21:13:45 +0100

    in the function get_all_users I added a condition to filter if user has lunch, also changed the post request, and it is now working with frontend

--- crud.py
+++ crud.py
@@ -1,31 +1,24 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session, joinedload
-from sqlalchemy import delete
+from sqlalchemy import or_, and_
 from models import User, Lunch
 import schemas
-from copy import deepcopy
-import logging
-
 def create_user(user: schemas.UserBase, db: Session):
     new_user = User(id=user.id, name=user.name)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    for lunch in user.lunches:
-        db_lunch = Lunch(type_of_lunch=lunch.type_of_lunch, owner_id=new_user.id)
+    db_lunch = Lunch(type_of_lunch=0, owner_id=new_user.id)
 
-        if int(db_lunch.type_of_lunch) > 3 or int(db_lunch.type_of_lunch) < 0:
-            raise HTTPException(status_code=500, detail="Invalid type of lunch.")
+    if int(db_lunch.type_of_lunch) > 3 or int(db_lunch.type_of_lunch) < 0:
+        raise HTTPException(status_code=500, detail="Invalid type of lunch.")
 
-        db.add(db_lunch)
+    db.add(db_lunch)
     db.commit()
-
     return new_user.id
 def update_lunch_for_user(user_id: str, type_of_lunch: int, db: Session):
-    # Retrieve the existing lunch for the user
     existing_lunch = db.query(Lunch).filter(Lunch.owner_id == user_id).first()
 
-    # Check if the user exists
     db_user = db.query(User).filter(User.id == user_id).first()
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -43,12 +36,12 @@ def update_lunch_for_user(user_id: str, type_of_lunch: int, db: Session):
 
     # Commit changes to the database
     db.commit()
-
 def get_user_by_id(db: Session, user_id: str):
     db_user = db.query(User).filter(User.id == user_id).options(joinedload(User.lunches)).first()
 
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
+
 
     existing_lunch = db.query(Lunch).filter(Lunch.owner_id == user_id).first()
 
@@ -69,21 +62,29 @@ def get_user_by_id(db: Session, user_id: str):
 def get_all_users_with_lunch_out(db: Session):
     db_users = db.query(User).filter(User.lunches.any(lunch_out=2)).options(joinedload(User.lunches)).all()
 
-
     if not db_users:
-        return "no users with lunch_out 2"
+        return 0
     return db_users
-
 def get_all_users(db: Session):
-    db_users = db.query(User).options(joinedload(User.lunches)).all()
+    db_users = db.query(User).filter(
+        and_ (
+            or_(
+                User.lunches.any(lunch_out=1),
+                User.lunches.any(lunch_out=2),
+            ),
+            or_(
+                User.lunches.any(type_of_lunch=1),
+                User.lunches.any(type_of_lunch=2),
+                User.lunches.any(type_of_lunch=3)
+            )
+        )
+    ).options(joinedload(User.lunches)).all()
 
     if not db_users:
-        return "no users"
+        return 0
     return db_users
-
 def delete_all_users(db: Session):
     try:
-        # Fetch all users
         users = db.query(User).all()
 
         # Delete related lunch records for each user
@@ -92,10 +93,7 @@ def delete_all_users(db: Session):
 
         # Delete all users
         db.query(User).delete()
-
-        # Commit the changes
         db.commit()
     except Exception as e:
-        # Rollback the transaction if an error occurs
         db.rollback()
         raise e
--- main.py
+++ main.py
@@ -14,13 +14,11 @@ origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=['*']
 )
-
-
 
 def get_db():
     db = SessionLocal()
@@ -30,22 +28,19 @@ def get_db():
         db.close()
 
 
-@app.post("/create/user/")
+@app.post("/create")
 async def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     return crud.create_user(user=user, db=db)
-
 
 @app.put("/users/{user_id}/update-lunch/{lunch_id}")
 async def update_user_lunch(user_id: str, type_of_lunch, db: Session = Depends(get_db)):
     return crud.update_lunch_for_user(user_id=user_id, type_of_lunch=type_of_lunch, db=db)
 
-
 @app.get("/users/{user_id}")
 async def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
     return crud.get_user_by_id(db=db, user_id=user_id)
 
-
-@app.get("/users")
+@app.get("/usersWithLunchOut")
 async def get_users_with_lunch_out(db: Session = Depends(get_db)):
     return crud.get_all_users_with_lunch_out(db=db)
 
