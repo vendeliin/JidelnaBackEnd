5d4c3624ee19dacd39aa3aed780ac43a5846c431
vendeliin <vendelinkukla@gmial.com>
2024-04-02 23:02:31 +0200

    Refactor endpoint URLs for consistency
    Updated the endpoint URLs in main.py file to make them more consistent.

--- app/main.py
+++ app/main.py
@@ -34,71 +34,71 @@ def get_db():
         db.close()
 
 
-@app.post("/create/user")
+@app.post("/users")
 async def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     return post.create_user(user=user, db=db)
 
 
-@app.post("/login")
+@app.post("/admins/login")
 async def login(user: schemas.AdminUserBase, db: Session = Depends(get_db)):
     return auth.authenticate_user(user=user, db=db)
 
 
-@app.post("/create/admin")
+@app.post("/admins")
 async def create_admin(user: schemas.AdminUserBase, db: Session = Depends(get_db)):
     return auth.create_user(user=user, db=db)
 
 
-@app.put("/user/{user_name}/update-lunch/{type_of_lunch}")
+@app.put("/users/{user_name}/lunches/{type_of_lunch}")
 async def update_user_lunch(user_name: str, type_of_lunch: int, db: Session = Depends(get_db)):
     return put.update_lunch_for_user(user_name=user_name, type_of_lunch=type_of_lunch, db=db)
 
 
-@app.get("/user/{user_id}")
+@app.get("/users/{user_id}")
 async def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
     return get.get_user_by_id(db=db, user_id=user_id)
 
 
-@app.get("/users/WithLunchOut")
+@app.get("/users/lunch/out")
 async def get_users_with_lunch_out(db: Session = Depends(get_db)):
     return get.get_all_users_with_lunch_out(db=db)
 
 
-@app.get("/users/All")
+@app.get("/users/lunch")
 async def get_users(db: Session = Depends(get_db)):
     return get.get_all_users_who_have_lunch(db=db)
 
 
-@app.get("/users/count")
+@app.get("/users/total")
 async def get_count_of_users(db: Session = Depends(get_db)):
     return get.get_count_of_users(db=db)
 
 
-@app.get("/lunches/count/rest")
+@app.get("/lunches/rest/total")
 async def get_lunches_count_rest(db: Session = Depends(get_db)):
     return get.get_count_of_lunches_rest(db=db)
 
 
-@app.get("/user/name/{user_id}")
+@app.get("/users/{user_id}/name")
 async def get_user_name(user_id: str, db: Session = Depends(get_db)):
     return get.get_user_name_by_id(user_id=user_id, db=db)
 
 
-@app.get("/lunches/count/out")
+@app.get("/lunches/out/total")
 async def get_lunches_count_out(db: Session = Depends(get_db)):
     return get.get_count_of_lunches_out(db=db)
 
 
-@app.delete("/users/delete")
+@app.delete("/users")
 async def delete_users(db: Session = Depends(get_db)):
     return delete.delete_all_users(db=db)
 
 
-@app.delete("/user/delete/{user_id}")
+@app.delete("/users/{user_id}")
 async def delete_user(user_id: str, db: Session = Depends(get_db)):
     return delete.delete_user(user_id=user_id, db=db)
 
 
-@app.delete("/user/delete/by/grade/{grade}")
+@app.delete("/user/grade/{grade}")
 async def delete_user_by_grade(grade: int, db: Session = Depends(get_db)):
     return delete.delete_users_by_grade(db=db, grade=grade)
